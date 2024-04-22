7509c15c3cc1967e479d280dfbd3cced99d223ae
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-22 18:58:46 -0500

    Ultimo commit

--- js/01.js
+++ js/01.js
@@ -5,3 +5,5 @@ var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
 var disponible; // Iniciamos la variable pero sin valor
 
 producto = true; // Reasignando el valor de la variable
+
+disponible = true; // Asignando el valor de la variable
