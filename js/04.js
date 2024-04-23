45cea9b7467f949e6eafe83542aa775dbae3a7a3
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-23 17:39:47 -0500

    Ultimo commit

--- js/04.js
+++ js/04.js
@@ -2,8 +2,16 @@
 
 const producto = "Monitor 20 Pulgadas";
 const producto2 = String('Monitor 30 Pulgadas'); // Esta es otra forma de crear un string
-const producto3 = new String('Monitor 50 Pulgadas'); // Esta es otra forma de crear un string
+const producto3 = new String('Monitor 50 Pulgadas'); // Este es un objeto, no un string
+const producto4 = "Monitor 20\" Pulgadas"; // Para usar comillas dobles dentro de un string, se usa el backslash
+const producto5 = 'Monitor HD"'; // 
+const tweet = 'Aprendiendo JavaScript con el curso de desarrollo web completo'; //
 
 console.log(producto);
 console.log(producto2);
+console.log(producto3); // Este es un objeto, no un string
+console.log(producto4); // Para usar comillas dobles dentro de un string, se usa el backslash
+console.log(producto5.length); // Para saber la longitud de un string, se usa la propiedad length
+console.log(tweet.length); // Para saber la longitud de un string, se usa la propiedad length
+
 
