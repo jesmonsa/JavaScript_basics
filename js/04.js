0c704e8401165feacce29ce5801bcdbed5d24c45
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-23 17:07:17 -0500

    Ultimo commit

---
+++ js/04.js
@@ -0,0 +1,9 @@
+// String o cadena de texto
+
+const producto = "Monitor 20 Pulgadas";
+const producto2 = String('Monitor 30 Pulgadas'); // Esta es otra forma de crear un string
+const producto3 = new String('Monitor 50 Pulgadas'); // Esta es otra forma de crear un string
+
+console.log(producto);
+console.log(producto2);
+
