e147f501b36f78a326d16904454d27b02ff53e7e
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-22 19:03:11 -0500

    Ultimo commit

--- js/01.js
+++ js/01.js
@@ -12,4 +12,9 @@ var producto1 = 'Computadora',
     disponible1 = true,
     categoria = 'Computadoras'; // Podemos iniciar multiples variables en una sola linea  y asignarles valor
 
-    
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // camelcase
+var NombreProducto = 'Monitor HD'; // pascal case
+var nombreproducto = 'Monitor HD'; // lowercase
+
