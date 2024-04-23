f31f74cf6772def2821e03d56bd3c78eb2d2b802
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-22 19:14:32 -0500

    Ultimo commit

---
+++ js/02.js
@@ -0,0 +1,35 @@
+// Variables con let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true; // Asignando el valor de la variable
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Podemos iniciar multiples variables en una sola linea  y asignarles valor
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // camelcase
+let NombreProducto = 'Monitor HD'; // pascal case
+let nombreproducto = 'Monitor HD'; // lowercase
+
+console.log(producto); // Mostrar en consola
+console.log(disponible); // Mostrar en consola
+console.log(producto1); // Mostrar en consola
+console.log(disponible1); // Mostrar en consola
+console.log(categoria); // Mostrar en consola
+console.log(nombre_producto); // Mostrar en consola
+console.log(nombreProducto); // Mostrar en consola
+console.log(NombreProducto); // Mostrar en consola
+console.log(nombreproducto); // Mostrar en consola
+
+
+
+
+
+
