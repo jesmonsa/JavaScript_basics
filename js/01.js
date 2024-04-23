83114760d54fccc2020023aecc19f83dd7591c6b
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-22 19:01:01 -0500

    Ultimo commit

--- js/01.js
+++ js/01.js
@@ -7,3 +7,9 @@ var disponible; // Iniciamos la variable pero sin valor
 producto = true; // Reasignando el valor de la variable
 
 disponible = true; // Asignando el valor de la variable
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Podemos iniciar multiples variables en una sola linea  y asignarles valor
+
+    
