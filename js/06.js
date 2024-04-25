b2ba76cf7c6ecc2b3b27376e2cd7a7dd6e70cb42
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-25 09:29:31 -0500

    Ultimo commit

--- js/06.js
+++ js/06.js
@@ -8,3 +8,53 @@ resultado = Math.PI;
 
 console.log(resultado);
 
+// Redondear
+resultado = Math.round(2.8); // Redondea hacia arriba si es mayor a 5
+console.log(resultado);
+
+resultado = Math.round(2.2); // Redondea hacia abajo si es menor a 5
+
+console.log(resultado);
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1); // Redondea hacia arriba
+console.log(resultado);
+
+// Redondear hacia abajo
+resultado = Math.floor(2.9); // Redondea hacia abajo
+console.log(resultado);
+
+// Raíz cuadrada
+resultado = Math.sqrt(144); // Raíz cuadrada
+console.log(resultado);
+
+// Valor absoluto
+resultado = Math.abs(-500); // Valor absoluto
+console.log(resultado);
+
+// Potencia
+resultado = Math.pow(2, 4); // Potencia
+console.log(resultado);
+
+// Mínimo
+resultado = Math.min(3, 5, 1, 12, -3); // Mínimo
+console.log(resultado);
+
+// Máximo
+resultado = Math.max(3, 5, 1, 12, -3); // Máximo
+console.log(resultado);
+
+// Aleatorio
+resultado = Math.random(); // Aleatorio
+console.log(resultado);
+
+// Aleatorio dentro de un rango
+resultado = Math.floor(Math.random() * 30); // Aleatorio dentro de un rango de 0 a 30 
+console.log(resultado);
+
+// Aleatorio dentro de un rango
+resultado = Math.floor(Math.random() * 100); // Aleatorio dentro de un rango de 0 a 100
+console.log(resultado);
+
+
+
