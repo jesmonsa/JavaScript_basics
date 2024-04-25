0c6b288dae09eaf0cf6d7450b19a62d8d3a20f8f
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-25 08:30:04 -0500

    Ultimo commit

--- js/05.js
+++ js/05.js
@@ -3,10 +3,16 @@
 const nombre = "Juan";
 const numero1 = 10;
 const numero2 = 20;
+const numero3 = 200.20;
+const numero4 = .102030;
+const numero5 = -3;
 
 console.log(nombre);
 console.log(numero1); 
 console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
 console.log(numero1 + numero2); // Suma
 console.log(numero1 - numero2); // Resta
 console.log(numero1 * numero2); // Multiplicación
