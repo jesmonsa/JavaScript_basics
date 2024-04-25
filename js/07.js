9e7100fb19fc6b1dfebc8c03e69ab235137829a5
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-25 09:38:28 -0500

    Ultimo commit

---
+++ js/07.js
@@ -0,0 +1,34 @@
+// Orden de las operaciones en JavaScript (precedencia de operadores)
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Operator_Precedence
+// https://www.w3schools.com/js/js_operators.asp
+// https://www.w3schools.com/js/js_arithmetic.asp
+// https://www.w3schools.com/js/js_assignment.asp
+// https://www.w3schools.com/js/js_comparison.asp
+// https://www.w3schools.com/js/js_logical.asp
+// https://www.w3schools.com/js/js_bitwise.asp
+// https://www.w3schools.com/js/js_type_conversion.asp
+// https://www.w3schools.com/js/js_string_methods.asp
+// https://www.w3schools.com/js/js_number_methods.asp
+// https://www.w3schools.com/js/js_math.asp
+// https://www.w3schools.com/js/js_random.asp
+// https://www.w3schools.com/js/js_date_methods.asp
+// https://www.w3schools.com/js/js_array_methods.asp
+// https://www.w3schools.com/js/js_array_sort.asp
+// https://www.w3schools.com/js/js_array_iteration.asp
+
+let resultado;
+
+resultado = 20 + 30 * 2; // 80
+console.log(resultado);
+
+resultado = (20 + 30) * 2; // 100
+console.log(resultado);
+
+resultado = (600 + 600) * 1.16; // 1392
+console.log(resultado);
+
+resultado = (100 + 200 + 300) * .2; // 120 descuento
+console.log(resultado);
+
+resultado = (100 + 200 + 300) * 1.16; // 696 con IVA
+console.log(resultado);
