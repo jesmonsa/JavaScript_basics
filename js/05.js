79e39e6d8112efb8211feb0bc96ed3e9988edfb9
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-25 08:22:41 -0500

    Ultimo commit

---
+++ js/05.js
@@ -0,0 +1,15 @@
+// Números y operadores
+
+const nombre = "Juan";
+const numero1 = 10;
+const numero2 = 20;
+
+console.log(nombre);
+console.log(numero1); 
+console.log(numero2);
+console.log(numero1 + numero2); // Suma
+console.log(numero1 - numero2); // Resta
+console.log(numero1 * numero2); // Multiplicación
+console.log(numero1 / numero2); // División
+console.log(numero1 % numero2); // Módulo
+console.log(numero1 ** numero2); // Potencia
