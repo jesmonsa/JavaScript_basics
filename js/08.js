fc690bfa20920cbdc0b584c13c9d7844451c4500
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-25 12:04:05 -0500

    Ultimo commit

---
+++ js/08.js
@@ -0,0 +1,11 @@
+const nombre = 'Juan';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log(nombre);
+console.log(email);
+console.log("Nombre del cliente: " + nombre + " Email: " + email);
+console.log("Nombre del cliente: " , nombre , " Email: " , email);
+
+// Template Strings
+console.log(`Nombre del cliente: ${nombre} Email: ${email}`); // Recomendado
