d51c8c7c4ed9a6f082ecf37c80a6373c08a57f40
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-26 16:42:35 -0500

    Ultimo commit

--- js/09.js
+++ js/09.js
@@ -5,4 +5,5 @@ const boolean2 = false;
 
 console.log(boolean1, boolean2);
 
-
+const boolean3 = new Boolean(true); // Object constructor for boolean values (not recommended) 
+console.log(boolean3); // [Boolean: true]
