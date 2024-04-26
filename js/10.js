449e4f427423c8daa5877c2e83044be43b397ac3
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-26 18:08:49 -0500

    Ultimo commit

--- js/10.js
+++ js/10.js
@@ -10,13 +10,22 @@ const producto = {
     disponible: true
 }
 
-console.log(producto); // { nombreProducto: 'Monitor de 20 pulgadas', precio: 300, disponible: true }
 
-console.log(producto.nombreProducto); // Monitor de 20 pulgadas
-console.log(producto.precio); // 300
-console.log(producto.disponible); // true
 
-console.log(producto["nombreProducto"]); // Monitor de 20 pulgadas
-console.log(producto["precio"]); // 300
-console.log(producto["disponible"]); // true
+// console.log(producto.nombreProducto); // Monitor de 20 pulgadas
+// console.log(producto.precio); // 300
+// console.log(producto.disponible); // true
 
+// console.log(producto["nombreProducto"]); // Monitor de 20 pulgadas
+// console.log(producto["precio"]); // 300
+// console.log(producto["disponible"]); // true
+
+// Agregar nuevas propiedades al objeto
+producto.imagen = "imagen.jpg";
+
+console.log(producto); // { nombreProducto: 'Monitor de 20 pulgadas', precio: 300, disponible: true, imagen: 'imagen.jpg' }
+
+// Eliminar propiedades del objeto
+delete producto.disponible;
+
+console.log(producto); // { nombreProducto: 'Monitor de 20 pulgadas', precio: 300, imagen: 'imagen.jpg' }
