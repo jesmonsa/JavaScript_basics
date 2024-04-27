fe24b830918ccaf257acd0757e348b0c6893fd13
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-27 11:36:17 -0500

    Ultimo commit

--- js/12.js
+++ js/12.js
@@ -1,5 +1,25 @@
+// "use strict";
+
 const producto = {
     nombreProducto: "Monitor de 20 pulgadas",
     precio: 300,
     disponible: true
 }
+
+// Object.freeze(producto); // No se puede modificar el objeto ni agregar ni eliminar propiedades
+
+// // Agragar nuevas propiedades
+// producto.imagen = 'imagen.jpg';
+
+// console.log(Object.isFrozen(producto));
+
+// console.log(producto);
+
+Object.seal(producto); // No se puede agregar ni eliminar propiedades, pero si modificar las existentes
+
+// Agragar nuevas propiedades
+producto.imagen = 'imagen.jpg';
+
+console.log(Object.isSealed(producto));
+
+console.log(producto);
