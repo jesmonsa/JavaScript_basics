fb61947449c87c0368a06409505a073266f869d0
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-27 21:29:15 -0500

    Ultimo commit

---
+++ js/15.js
@@ -0,0 +1,11 @@
+// Arreglos o arrays
+
+// Crear un arreglo o array con valores numericos cuando vemos corchetes [] es un array
+const numeros = [10, 20, 30, 40, 50];
+
+console.log(numeros);
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+console.log(meses);
+
