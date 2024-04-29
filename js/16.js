4c4907e223e0752d848f805e475e0e072b91e24e
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-28 20:29:22 -0500

    Ultimo commit

--- js/16.js
+++ js/16.js
@@ -1,1 +1,27 @@
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Television', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+    { nombre: 'Bocinas', precio: 800},
+    { nombre: 'Laptop', precio: 900},
+];
+
+// ForEach
+meses.forEach(function(mes) {
+    console.log(mes);
+});
+
+// Estrucutra de un control forEach
+
+meses.forEach(function(mes) {
+    if (mes === 'Marzo') {
+        console.log('Marzo si existe');
+    }
+});
+
+
