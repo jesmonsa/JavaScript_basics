bdf2884b2045e1d3440949f3a254e8e6c33fba90
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-30 08:07:46 -0500

    Ultimo commit

---
+++ js/17.js
@@ -0,0 +1,12 @@
+// Funciones son reutilizables
+
+// Declaración de la función
+
+function sumar (a, b) {
+    return a + b;
+}
+
+// Llamada a la función
+
+let resultado = sumar(1, 2);
+console.log(resultado);
