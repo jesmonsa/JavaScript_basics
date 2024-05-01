24f7c1e560cc82db7c99c0643c0f86f15d93c4b1
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-01 14:02:26 -0500

    Ultimo commit

--- js/20.js
+++ js/20.js
@@ -1,8 +1,30 @@
 // Funciones que retornan valores 
 
-function sumar(a, b) {
-    return a + b;
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+// let resultado = sumar(1, 2);
+// console.log(resultado);
+
+let total = 0;
+
+function agregarCarrito(precio) {
+    return total += precio;
 }
 
-let resultado = sumar(1, 2);
-console.log(resultado);
+function calcularImpuesto(total) {
+    return 1.15 * total;
+}
+
+total = agregarCarrito(300);
+total = agregarCarrito(100);
+total = agregarCarrito(600);
+
+console.log(total); // 1000
+
+const totalPagar = calcularImpuesto(total);
+
+console.log(`Total a pagar con impuestos: ${totalPagar}`); // 1150
+
+
