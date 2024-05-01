3036799b2c4091b71db5e2916a6b44ef23d4f4a1
Jesus Montoya <jesmonsa2018@gmail.com>
2024-04-30 19:12:06 -0500

    Ultimo commit

--- js/17.js
+++ js/17.js
@@ -2,7 +2,18 @@
 
 // Declaración de la función
 
-function sumar (a, b) {
+// function sumar (a, b) {
+//     return a + b;
+// }
+
+// // Llamada a la función
+
+// let resultado = sumar(1, 2);
+// console.log(resultado);
+
+// Expresión de la función
+
+const sumar = function (a, b) {
     return a + b;
 }
 
@@ -10,3 +21,10 @@ function sumar (a, b) {
 
 let resultado = sumar(1, 2);
 console.log(resultado);
+
+// IIFE (Inmediately Invoked Function Expression) - Funciones que se invocan inmediatamente
+
+(function (a, b) {
+    console.log(a + b);
+})(3, 4);
+
