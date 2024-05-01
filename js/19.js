a0989d74873b173f97c85c850196b9b6d6566668
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-01 13:49:20 -0500

    Ultimo commit

---
+++ js/19.js
@@ -0,0 +1,16 @@
+
+function sumar(numero1 = 0, numero2 = 0) {
+  console.log(numero1 + numero2);
+}
+
+sumar( 2, 3 );
+sumar(3, 5);
+sumar(10, 10);
+sumar(10);
+
+
+const sumar2 = function(n1, n2) {
+  console.log(n1 + n2);
+}
+
+sumar2(5, 5);
