70d34c894b9501e47b00b4cdee3e40a47da4f046
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-03 19:00:54 -0500

    Ultimo commit

---
+++ js/24.js
@@ -0,0 +1,19 @@
+// Switch
+
+const metodoPago = 'cheque';
+
+switch(metodoPago) {
+    case 'efectivo':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    case 'cheque':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    case 'tarjeta':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    default:
+        console.log('Aún no has pagado');
+        break;
+}
+
