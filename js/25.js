61fab72e578755535d722d231081d880e7ec8adc
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-03 19:21:16 -0500

    Ultimo commit

--- js/25.js
+++ js/25.js
@@ -5,3 +5,13 @@
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+// Par o Impar
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} es Par`);
+    } else {
+        console.log(`${i} es Impar`);
+    }
+}
