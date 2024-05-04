ee66c16182a5f5e808a4f878e6b531a0c2350285
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-04 12:25:02 -0500

    Ultimo commit

--- js/28.js
+++ js/28.js
@@ -1,1 +1,22 @@
 // Object constructor and obejcet literal
+// POO
+
+// Object literal
+const producto = {
+    nombre: 'Monitor 20 Pulgadas',
+    precio: 300,
+    disponible: true,
+    mostrarInfo: function() {
+        console.log(`El producto: ${this.nombre} tiene un precio de: ${this.precio}`);
+    }
+}
+
+producto.mostrarInfo();
+
+// Object Constructor
+function Producto(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = true;
+}
+
