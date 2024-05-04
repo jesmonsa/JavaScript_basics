d26ee95e4412317c22aa197d7d3b56a986a9087a
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-04 06:39:21 -0500

    Ultimo commit

--- js/25.js
+++ js/25.js
@@ -30,3 +30,45 @@ const carrito = [
 for (let i = 0; i < carrito.length; i++) {
     console.log(carrito[i].nombre);
 }
+
+// While Loop
+
+let i = 1; // Inicializar el While Loop
+
+while (i < 10) { // Condicion del While Loop
+    if (i % 2 === 0) {
+        console.log(`${i} es Par`);
+    } else {
+        console.log(`${i} es Impar`);
+    }
+    i++;
+}
+
+// While Loop con Array
+
+let j = 0; // Inicializar el While Loop
+
+while (j < carrito.length) { // Condicion del While Loop
+    console.log(carrito[j].nombre);
+    j++;
+}
+
+// Do While Loop
+
+let k = 0; // Inicializar el Do While Loop
+
+do {
+    console.log(k);
+    k++;
+} while (k < 10); // Condicion del Do While Loop
+
+// Do While Loop con Array
+
+let l = 0; // Inicializar el Do While Loop
+
+do {
+    console.log(carrito[l].nombre);
+    l++;
+} while (l < carrito.length); // Condicion del Do While Loop
+
+
