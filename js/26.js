59d37046bd66c87f32bb76fb0d4c40014b739296
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-04 06:43:47 -0500

    Ultimo commit

---
+++ js/26.js
@@ -0,0 +1,13 @@
+// forEach y map para recorrer arreglos
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Television', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+    { nombre: 'Bocinas', precio: 800 },
+    { nombre: 'Laptop', precio: 900 }
+
+];
