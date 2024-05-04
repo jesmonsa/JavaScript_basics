8a5ee7d1d07bd42b184a5e9d3319184766bdbf7a
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-04 12:19:12 -0500

    Ultimo commit

--- js/27.js
+++ js/27.js
@@ -1,1 +1,14 @@
 // This 
+
+const reservacion = {
+    nombre: 'Juan',
+    apellido: 'De la torre',
+    total: 5000,
+    pagado: false,
+    informacion: function() {
+        console.log(`El cliente ${this.nombre} reservo y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+reservacion.informacion();
+
