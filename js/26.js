f09d1669dbc8946eafae39402dbf9ac24bd45921
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-04 06:53:08 -0500

    Ultimo commit

--- js/26.js
+++ js/26.js
@@ -9,5 +9,31 @@ const carrito = [
     { nombre: 'Celular', precio: 700 },
     { nombre: 'Bocinas', precio: 800 },
     { nombre: 'Laptop', precio: 900 }
-
 ];
+
+// forEach
+
+carrito.forEach(producto => console.log(producto.nombre));
+
+// map
+
+const arreglo2 = carrito.map(producto => producto.nombre);
+console.log(arreglo2);
+
+// ForEach y Map con Objetos
+
+const persona = {
+    nombre: 'Juan',
+    profesion: 'Desarrollador Web',
+    edad: 500
+}
+
+Object.keys(persona).forEach(key => {
+    console.log(key);
+});
+
+Object.values(persona).forEach(value => {
+    console.log(value);
+});
+
+
