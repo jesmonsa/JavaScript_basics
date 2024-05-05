38688310fed54be5b86df1caea61154780f21ecb
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-05 16:03:43 -0500

    Ultimo commit

--- js/28.js
+++ js/28.js
@@ -20,6 +20,25 @@ function Producto(nombre, precio) {
     this.disponible = true;
 }
 
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
 const producto2 = new Producto('Monitor 24 Pulgadas', 500);
+const producto3 = new Producto('Television 50 Pulgadas', 700);
+
+const cliente = new Cliente('Juan', 'Herrera');
+
+console.log(cliente);
+
+function formatearProducto(producto) {
+    return `El producto ${producto.nombre} tiene un precio de: ${producto.precio}`;
+}
+
+
 console.log(producto2);
+console.log(producto3);
+
+console.log(formatearProducto(producto2));
 
