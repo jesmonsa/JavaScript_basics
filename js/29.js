44de2dc883e1b18d07f7230d8414e1f697b12fa1
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-05 18:31:24 -0500

    Ultimo commit

--- js/29.js
+++ js/29.js
@@ -9,14 +9,23 @@ class Producto {
     formatearProducto() {
         return `El producto ${this.nombre} tiene un precio de: ${this.precio}`;
     }
-
-    obtenerPrecio() {
-        console.log(this.precio);
-    }
 }
 
 const producto2 = new Producto('Monitor 24 Pulgadas', 500);
 const producto3 = new Producto('Television 50 Pulgadas', 700);
 
+// Herencia
+class Libro extends Producto {
+    constructor(nombre, precio, isbn) {
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+    formatearProducto() {
+        return `${super.formatearProducto()} y su ISBN es ${this.isbn}`;
+    }
+}
+
+const libro = new Libro('JavaScript la Revolución', 120, '123456');
+console.log(libro.formatearProducto());
 console.log(producto2);
 console.log(producto3);
