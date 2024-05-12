370ac2cb74b8c235abf175cd5ad9f6ea51c3f232
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-12 18:58:09 -0500

    Ultimo commit

--- js/32.js
+++ js/32.js
@@ -1,2 +1,6 @@
 
-const 
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', function() {
+    Notification.requestPermission()
+        .then(resultado => console.log(`El resultado es: ${resultado}`));
+});
