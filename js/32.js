63559fc6d34fd778e1d9a3c8fd3e20f8a5490a00
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-12 21:02:43 -0500

    Ultimo commit

--- js/32.js
+++ js/32.js
@@ -4,3 +4,15 @@ boton.addEventListener('click', function() {
     Notification.requestPermission()
         .then(resultado => console.log(`El resultado es: ${resultado}`));
 });
+
+
+if(Notification.permission === 'granted') {
+    new Notification('Esta es una notificación', {
+        icon: 'img/ccj.png',
+        body: 'Código con Juan, aprende con proyectos reales',
+    });
+} else if(Notification.permission === 'denied') {
+    console.log('Permiso denegado');
+} else {
+    console.log('Permiso no otorgado');
+}
