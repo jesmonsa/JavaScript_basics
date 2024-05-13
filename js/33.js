4f19ea154cabb34c9aa0b73387de76719af03c2f
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-12 21:28:41 -0500

    Ultimo commit

--- js/33.js
+++ js/33.js
@@ -10,16 +10,37 @@ function descargarClientes() {
             } else {
                 reject('Error en la conexión');
             }
+        }, 5000);
+    });
+}
+
+function descargarPedidos() {
+    return new Promise((resolve, reject) => {
+        const error = false;
+
+        setTimeout(() => {
+            if (!error) {
+                resolve('El listado de pedidos se descargó correctamente');
+            } else {
+                reject('Error en la conexión');
+            }
         }, 3000);
     });
 }
 
 async function ejecutar() {
     try {
-        console.log(1 + 1);
-        const respuesta = await descargarClientes();
-        console.log(2 + 2);
-        console.log(respuesta);
+        // console.log(1 + 1);
+        // const respuesta = await descargarClientes();
+        // const respuesta2 = await descargarPedidos();
+        // console.log(2 + 2);
+        // console.log(respuesta);
+        // console.log(respuesta2);
+
+        const respuesta = await Promise.all([descargarClientes(), descargarPedidos()]);
+        console.log(respuesta[0]);
+        console.log(respuesta[1]);
+        
     } catch (error) {
         console.log(error);
     }
